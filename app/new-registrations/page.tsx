cfb96444c7c5a6c96e2a803cab399c288621d8a5
Kevin Bervoets <kevin.bervoets@proton.me>
2026-08-02 22:36:33 +0200

    add nav item for new registrations overview

---
+++ app/new-registrations/page.tsx
@@ -0,0 +1,9 @@
+import BasicTemplate from "@/components/templates/basic-template";
+
+export default function NewRegistrations() {
+  return (
+    <BasicTemplate>
+      <h1>Camino Nieuwe Aanvragen</h1>
+    </BasicTemplate>
+  );
+}
